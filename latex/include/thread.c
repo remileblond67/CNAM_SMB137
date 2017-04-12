1cd3d73d2c9c8638ba4506e770d233ede3ed3987
Rémi LEBLOND <leblond.remi@gmail.com>
2017-04-12 13:57:44 +0200

    Ajout d'une variable globale dans l'exemple des threads

--- latex/include/thread.c
+++ latex/include/thread.c
@@ -2,25 +2,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-
-void * thread_1 (void *arg) {
-  printf ("***** Nous sommes dans le thread *****\n");
+int i; // Variable globale
+void * thread_1 (void *arg) {  // Fonction du thread
+  printf ("*** Dans le thread, i=%d ***\n", i);
   pthread_exit (NULL);
 }
-
-int main (void) {
+int main (void) {              // Fonction principale
   pthread_t thread1;
   printf ("Avant la création du thread.\n");
   if (pthread_create (&thread1, NULL, thread_1, NULL)) {
       perror ("pthread_create");
-      return EXIT_FAILURE;
-    }
-  for (int i = 0; i < 10000000; i++) {
-      if (i % 1000000 == 0) printf ("Suite du processus principal...\n");
-    }
-  // On attend la fin du thread
+      return EXIT_FAILURE; }
+  for (i = 0; i < 10000000; i++) {
+      if (i % 1000000 == 0) printf ("Suite du processus principal...\n"); }
   if (pthread_join (thread1, NULL)) {
-      perror ("pthread_join");
+      perror ("pthread_join"); // On attend la fin du thread
       return EXIT_FAILURE;
     }
   printf ("Fin du processus principal.\n");
